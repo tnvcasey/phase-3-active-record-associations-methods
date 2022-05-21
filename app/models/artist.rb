69cd85462626d03123b12138c112ac41df9d5e5a
taylorvercillo <taylornvcasey@gmail.com>
2022-05-20 22:04:49 -0600

    commit tree 0180d837619710c3c0a650fd4a297f72a186bee9

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    self.genre(1)
   end
 
   def song_count
@@ -11,6 +11,6 @@ class Artist < ActiveRecord::Base
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    self.genres
   end
 end
